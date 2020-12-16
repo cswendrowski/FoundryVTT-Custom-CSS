18a2d7f7b1ae91452aace74dd1801f5e20eb4ff9
zeel <zeel02@gmail.com>
2020-12-16 16:59:14 -0500

    Normalized indentation

--- custom-css.js
+++ custom-css.js
@@ -2,34 +2,35 @@
 
 import { Settings } from "./settings.js";
 
+
 Hooks.once('init', async function() {
-  const version = 1.0;  //Current Version
+    const version = 1.0;  //Current Version
 
-  //Bootstrap
-  if (!window.CustomCss) {
-    window.CustomCss = { loaded: 0 };
-    window.CustomCss.setup = () => console.error('CustomCss | Failed to setup CustomCss');
-    $(() => window.CustomCss.setup());
-  }
-
-  window.CustomCss.loaded = version;
-
-  window.CustomCss.setup = () => {
-    console.log(`CustomCss | Initializing v` + version);
-    Settings.registerSettings();
-
-    const style = document.createElement("style");
-    style.id = "CustomCSS";
-    document.querySelector("head").appendChild(style);
-
-    let css = "";
-    for(let rule of Settings.rules()) {
-      if (rule == "" || rule == "<DELETED>") continue;
-      css += rule;
+    //Bootstrap
+    if (!window.CustomCss) {
+        window.CustomCss = { loaded: 0 };
+        window.CustomCss.setup = () => console.error('CustomCss | Failed to setup CustomCss');
+        $(() => window.CustomCss.setup());
     }
 
-    style.innerHTML = css;
+    window.CustomCss.loaded = version;
 
-     console.log(css);
-  };
+    window.CustomCss.setup = () => {
+        console.log(`CustomCss | Initializing v` + version);
+        Settings.registerSettings();
+
+        const style = document.createElement("style");
+        style.id = "CustomCSS";
+        document.querySelector("head").appendChild(style);
+
+        let css = "";
+        for(let rule of Settings.rules()) {
+            if (rule == "" || rule == "<DELETED>") continue;
+            css += rule;
+        }
+
+        style.innerHTML = css;
+
+         console.log(css);
+    };
 });
